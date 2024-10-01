54f05976ebb5f4b6ba4644046159956257b822e7
zavaultraz <zavazaneta@gmail.com>
2024-10-01 22:34:22 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,22 @@
-@extends('admin.include.parent');
+@extends('admin.include.parent')
 @section('content')
-<style>
-    * {color: #012970;}
-</style>
-<div class="container  card p-4">
-    <div class="row "   >
-        <h1>Wellcome {{ Auth::user()->name }} <i class="bi bi-person-check-fill"></i></h1>
+
+<div class="card">
+    <div class="container p-4">
+        <h1 class="text-center">Welcome, {{ Auth::user()->name }} <i class="bi bi-person-check-fill"></i></h1>
         <hr>
-        <div class="card p-2">
-            <div class="text-center list-group-item">
-                <h2 class="fw-bold fs-2" style="color: #012970;">Semangat 🫡</h2>
-                <h1><strong><span id="clock"></span></strong></h1>
-                </div>
+
+        <div class="card p-2 text-center">
+            <h2 class="fw-bold fs-2" style="color: #012970;">Semangat 🫡</h2>
+            <h1><strong><span id="clock"></span></strong></h1>
         </div>
-        <!-- tambahkan jam di sini -->
-        <div class="p-2">
-            <h3 class="text-center p-2 text-uppercase"><strong>{{ Auth::user()->name }} INFO <i class="bi bi-person-circle"></i></strong></h3>
+
+        <div class="p-2 mt-4">
+            <h3 class="text-center text-uppercase"><strong>{{ Auth::user()->name }} INFO <i class="bi bi-person-circle"></i></strong></h3>
             <ul class="list-group">
-                <li class="list-group-item" aria-current="true">Username : <strong>{{ Auth::user()->name }}</strong></li>
-                <li class="list-group-item">Email : <strong>{{ Auth::user()->email }}</strong></li>
-                <li class="list-group-item">Title : <strong>{{ Auth::user()->role }}</strong></li>
-                <!-- Tambahkan div untuk menampilkan jam -->
-                
+                <li class="list-group-item" aria-current="true">Username: <strong>{{ Auth::user()->name }}</strong></li>
+                <li class="list-group-item">Email: <strong>{{ Auth::user()->email }}</strong></li>
+                <li class="list-group-item">Title: <strong>{{ Auth::user()->role }}</strong></li>
             </ul>
         </div>
     </div>
